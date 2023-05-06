baf71729bf2e52325c8c35a583b412d3f2cfa568
Caj Neubauer <123caj@gmail.com>
2023-05-06 10:08:10 -0600

    Added one test for each of the new functions

--- tests/testthat/test_dual_inlet_functions.R
+++ tests/testthat/test_dual_inlet_functions.R
@@ -5,3 +5,21 @@ base_dir <- if (interactive()) file.path("tests", "testthat") else "."
 
 context("dual inlet functions")
 
+test_that("orbi_define_dualInlet() tests", {
+
+  # safety checks
+
+  expect_error(orbi_define_dualInlet(), "no value for the number of infusion blocks provided",
+               fixed = TRUE)
+
+})
+
+
+test_that("orbi_annotate_dualInlet() tests", {
+
+  # safety checks
+
+  expect_error(orbi_annotate_dualInlet(), "no data provided",
+               fixed = TRUE)
+
+})
