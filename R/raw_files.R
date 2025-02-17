3c87ffa768036e4246319d26f2a4df19ac69538a
Sebastian Kopf <sebastian.kopf@colorado.edu>
2025-02-17 14:06:21 +0100

    parse raw fields

--- R/raw_files.R
+++ R/raw_files.R
@@ -1,5 +1,5 @@
 # read several raw files with progress bar and summary stats
-read_raw_files <- function(file_paths, show_progress = rlang::is_interactive(), show_problems = TRUE) {
+orbi_read_raw <- function(file_paths, show_progress = rlang::is_interactive(), show_problems = TRUE) {
   
   # get started
   n_files <- length(file_paths)
@@ -7,6 +7,7 @@ read_raw_files <- function(file_paths, show_progress = rlang::is_interactive(), 
   start <- Sys.time()
   
   # progress bar
+  pb <- NULL
   if (show_progress) { 
     old <- options(cli.progress_show_after = 0); on.exit(options(old))
     pb_multiplier <- 10 # for subprocesses
@@ -85,6 +86,7 @@ read_raw_files <- function(file_paths, show_progress = rlang::is_interactive(), 
 }
 
 # raw file reader
+# FIXME: should this really have it's own pb generation? probably not since it's a lower level function
 read_raw_file <- function(file_path, show_progress = rlang::is_interactive(), show_problems = TRUE, info = NULL, pb = NULL) {
   
   # safety checks
@@ -109,22 +111,84 @@ read_raw_file <- function(file_path, show_progress = rlang::is_interactive(), sh
     cli_progress_update(id = pb, status = "initializing")
   }
   
-  # read components
-  if (show_progress) cli_progress_update(id = pb, status = "headers")
+  # headers
+  if (show_progress) cli_progress_update(id = pb, status = "read headers")
   headers <- try_catch_cnds(rawrr::readFileHeader(file_path), error_value = tibble())
+  parse_headers <- function() convert_list_to_tibble(headers$result)
+  headers_parsed <- try_catch_cnds(parse_headers(), error_value = tibble())
   
+  # indices
   if (show_progress)  cli_progress_update(id = pb, status = "indices")
   indices <- try_catch_cnds(rawrr::readIndex(file_path), error_value = tibble(scan = integer(0)))
+  parse_indices <- function() {
+    if (!"scan" %in% names(indices$result)) rlang::abort("no {.var scan} column found in scan index")
+    indices$result$scan <- as.integer(indices$result$scan)
+    return(tibble::as_tibble(indices$result))
+  }
+  indices_parsed <- try_catch_cnds(parse_indices(), error_value =tibble(scan = integer(0)))
   
-  
+  # spectra
   if (show_progress) cli_progress_update(id = pb, status = "spectra")
-  spectra <- try_catch_cnds(test_whatup(), error_value = tibble())
-  #spectra <- try_catch_cnds(rawrr::readSpectrum(file_path, scan = indices$result$scan), error_value = tibble(), show_cnds = show_problems)
+  spectra <- try_catch_cnds(rawrr::readSpectrum(file_path, scan = indices$result$scan), error_value = list())
   
-  Sys.sleep(1)
+  # parse spectral data
+  if (show_progress) cli_progress_update(id = pb, status = "parse spectra")
+  parse_spectra <- function() {
+    spectra <- spectra$result |> map(
+      function(spectrum) {
+        if (!"scan" %in% names(spectrum)) rlang::abort("no {.var scan} column found in spectrum")
+        spectrum$scan <- as.integer(spectrum$scan)
+        convert_list_to_tibble(spectrum)
+      })
+    dplyr::bind_rows(spectra)
+  }
+  spectra_parsed <- try_catch_cnds(parse_spectra(), error_value = tibble(scan = integer(0)))
+  
+  # scan info
+  if (show_progress) cli_progress_update(id = pb, status = "parse scan info")
+  parse_scan_info <- function() {
+    scan_info_cols <- !str_detect(names(spectra_parsed$result), "^(centroid\\.|mZ|intensity)")
+    scan_info <- spectra_parsed$result[, scan_info_cols]
+    scan_cols <- setdiff(names(indices_parsed$result), names(scan_info))
+    indices_parsed$result[c("scan", scan_cols)] |> 
+      dplyr::left_join(scan_info, by = "scan")
+  }
+  scan_info <- try_catch_cnds(parse_scan_info(), error_value = tibble(scan = integer(0)))
+  
+  # peak table
+  if (show_progress) cli_progress_update(id = pb, status = "parse peak table")
+  parse_peak_table <- function() {
+    peak_cols <- str_detect(names(spectra_parsed$result), "^centroid\\.")
+    if (!any(peak_cols))
+      rlang::abort("no centroid data found in spectra, returning empty peak table")
+    peak_table <-
+      spectra_parsed$result[names(spectra_parsed$result) == "scan" | peak_cols] |> 
+      tibble::as_tibble()
+    peak_table |> tidyr::unnest(-"scan")
+  }
+  peak_table <- try_catch_cnds(parse_peak_table(), error_value = tibble(scan = integer(0)))
+  
+  # raw data
+  if (show_progress) cli_progress_update(id = pb, status = "parse raw data")
+  parse_raw_data <- function() {
+    data_cols <- str_detect(names(spectra_parsed$result), "^(mZ|intensity)")
+    if (!any(data_cols))
+      rlang::abort("no raw data ({.var mZ} & {.var intensity}) found in spectra, returning empty raw data table")
+    raw_data <-
+      spectra_parsed$result[names(spectra_parsed$result) == "scan" | data_cols] |> 
+      tibble::as_tibble()
+    raw_data |> tidyr::unnest(-"scan")
+  }
+  raw_data <- try_catch_cnds(parse_raw_data(), error_value = tibble(scan = integer(0)))
+  
+  # problems
+  problems <- bind_rows(
+    headers$conditions, headers_parsed$conditions, 
+    indices$conditions, indices_parsed$conditions,
+    spectra$conditions, spectra_parsed$conditions,
+    scan_info$conditions, peak_table$conditions, raw_data$conditions)
   
   # info
-  problems <- bind_rows(headers$conditions, indices$conditions, spectra$conditions)
   if (show_progress && new_pb) cli_process_done(id = pb)
   info <- format_inline(
     if (nrow(problems) == 0) "{col_green(symbol$tick)} " else "{col_green(symbol$info)} ",
@@ -143,9 +207,17 @@ read_raw_file <- function(file_path, show_progress = rlang::is_interactive(), sh
   # return
   tibble(
     file_path = file_path, 
-    headers = list(headers$result),
-    indices = list(indices$result),
-    spectra = list(spectra$result),
+    file_info = list(headers_parsed$result),
+    scans = list(scan_info$result),
+    peak_table = list(peak_table$result),
+    raw_data = list(raw_data$result),
     problems = list(problems)
   )
+}
+
+# turns multi value entries into list columns and omits NULL values
+convert_list_to_tibble <- function(file_info_list) {
+  lens <- lengths(file_info_list)
+  file_info_list[lens > 1] <- lapply(file_info_list[lens > 1], list)
+  return(tibble::as_tibble(file_info_list[lens > 0]))
 }
