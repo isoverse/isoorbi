06b3b073daf8689c35192e363dcc64a82810d062
Sebastian Kopf <seb.kopf@gmail.com>
2026-09-17 17:08:07 -0600

    cleanup use of ggplot2's %+% (no longer supported/needed)

--- R/isoorbi-package.R
+++ R/isoorbi-package.R
@@ -13,7 +13,7 @@
 #' @importFrom stats setNames
 #' @importFrom tibble is_tibble
 #' @importFrom utils data
-#' @importFrom ggplot2 %+% scale_color_manual scale_fill_manual
+#' @importFrom ggplot2 scale_color_manual scale_fill_manual
 #' @importFrom knitr knit_print
 ## usethis namespace: end
 NULL
